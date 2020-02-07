42b42b1e3069a94cdeae7261c24749eaebf39c10
Alexandr-Fox <al.rostilismith@yandex.ru>
2020-02-07 16:56:25 +0500

    Updates

---
+++ precache-manifest.21af23f5634ad0ea159852847f5ec1a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478e534abb6e0f259436e13b929e022d",
+    "url": "/Newsify/index.html"
+  },
+  {
+    "revision": "f68b6cd16e1ea33d6ab5",
+    "url": "/Newsify/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "d4af8de7066b94f50343",
+    "url": "/Newsify/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f68b6cd16e1ea33d6ab5",
+    "url": "/Newsify/static/js/2.675f3392.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Newsify/static/js/2.675f3392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4af8de7066b94f50343",
+    "url": "/Newsify/static/js/main.35dca738.chunk.js"
+  },
+  {
+    "revision": "fb0009ea22bb4aacbe1f",
+    "url": "/Newsify/static/js/runtime-main.5579cca6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Newsify/static/media/persik.4e1ec840.png"
+  }
+]);
