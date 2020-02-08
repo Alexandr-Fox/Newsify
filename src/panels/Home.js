227dbff8ea8a692bd0f4bd34e8df597ab0e91a34
Alexandr-Fox <al.rostilismith@yandex.ru>
2020-02-08 14:09:47 +0500

    изменено:      package.json 	изменено:      src/App.js 	изменено:      src/index.js 	изменено:      src/panels/Home.js 	новый файл:    src/panels/Settings.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import connect from '@vkontakte/vk-connect';
+//import connect from '@vkontakte/vk-connect';
+import VKConnect from '@vkontakte/vkui-connect-mock';
+import { platform, IOS } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
@@ -8,10 +10,20 @@ import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
+import Icon28Settings from '@vkontakte/icons/dist/28/settings';
+import Icon24Settings from '@vkontakte/icons/dist/24/settings';
+
+
+const osName = platform();
 
 const Home = ({ id, go, fetchedUser}) => (
 	<Panel id={id}>
-		<PanelHeader>Newsify</PanelHeader>
+		<PanelHeader
+			left={<HeaderButton onClick={go} data-to="settings">
+				{osName === IOS ? <Icon28Settings/> : <Icon24Settings/>}
+			</HeaderButton>}
+		>Newsify</PanelHeader>
 		{fetchedUser &&
 		<Group title="Профиль">
 			<Cell
@@ -47,22 +59,22 @@ Home.propTypes = {
 function City(fetchedUser){
     var town=fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : '';
 //    var town;
-    connect.subscribe(event => {
-        if (!event.detail) {
-            return;
-        }
+    VKConnect.subscribe(event => {
+//        if (!event.detail) {
+//            return;
+//        }
 
         const { type, data } = event.detail;
 
         if (type === 'VKWebAppGeodataResult') {
-            if( data.available===1){
+            if( data.available===true){
                 var adress='https://geocode-maps.yandex.ru/1.x/?format=json&apikey=aaa60bd2-f573-4cf9-873e-589107560bc0&sco=longlat&kind=locality&geocode='+data.long+','+data.lat;
                 var x=new XMLHttpRequest();
                 x.open('GET', adress,true);
                 x.onload = function() {
                     if (x.status === 200) {
                         var string = JSON.parse(x.responseText);
-                        town = string.response.GeoObjectCollection.featureMember.GeoObject[0].GeoObject.name;
+                        town = string['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['name'];
                     }
                 }
                 x.send()
@@ -71,8 +83,8 @@ function City(fetchedUser){
         }
         else {town = type;}
     });
-    connect.send("VKWebAppGetGeodata", {});
-//    town=JSON.stringify(connect.send("VKWebAppGetGeodata", {}));
+    town = VKConnect.send("VKWebAppGetGeodata", {});
+//    town=JSON.stringify(VKConnect.send("VKWebAppGetGeodata", {}));
     return town;
 }
 export default Home;
