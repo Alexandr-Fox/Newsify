dc15f726046a0b1ba836302e5344082e3cc1d72e
Alexandr-Fox <al.rostilismith@yandex.ru>
2020-02-19 19:35:14 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Newsify/precache-manifest.a82f36f1ee84e07ef4dad15d940b3374.js"
+  "/Newsify/precache-manifest.716b95aceda0bdb5f69e3ae86821ac1c.js"
 );
 
 self.addEventListener('message', (event) => {
